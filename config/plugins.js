6fa5108ea03aa0dfda7b7ab7c9b502b9e3760417
José Izaguirre <joseadolfoizaguirreflores@gmail.com>
2023-09-28 21:56:10 -0600

    test deploy

--- config/plugins.js
+++ config/plugins.js
@@ -16,5 +16,14 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  'schemas-to-ts': {
+    enabled: true,
+    config: {
+      acceptedNodeEnvs: ["development"],
+      commonInterfacesFolderName: "schemas-to-ts",
+      verboseLogs: false,
+      alwaysAddEnumSuffix: false
+    }
+  },
   // ...
 });
